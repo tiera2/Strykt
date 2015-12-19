ec00334acadfd7b7c7eb4d7a95995839169f2186
git <tobi.eriksson@gmail.com>
2015-12-19 14:00:48 +0100

    Lägger upp Slim Framework och api-folder
    Signed-off-by: git <tobi.eriksson@gmail.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/api/matches/', function (Request $request, Response $response) {
+	$response = $response->withHeader('Content-type', 'application/json');
+	require 'api/cMatchesApi.php';
+	$response->getBody()->write(MatchesApi::getCsv());
+	return $response;
+});
+
+$app->get('/', function (Request $request, Response $response) {
+	$response->getBody()->write("kör denna för att hämta matcher <a href='./api/matches/'>länk</a>");
+	return $response;
+});
+$app->run();
+
